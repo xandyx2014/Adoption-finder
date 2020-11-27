6f884121a5f86fd42469ad7f7e9ea95b790050bd
xandyx2014 <xandyx2014@gmail.com>
2020-11-26 22:26:15 -0400

    Agregado la edicion de usuario de mascota

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Perfil;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10);
+        \App\Models\Perfil::factory(10)->create();
         \App\Models\User::factory(1)->create(
             [
                 'email' => 'xandyx2014@gmail.com'
@@ -24,13 +25,13 @@ class DatabaseSeeder extends Seeder
         \App\Models\Especie::factory(5)->create();
         \App\Models\Raza::factory(5)
             ->create();
-        \App\Models\Etiqueta::factory(20)->create();
-        \App\Models\TipoDenuncia::factory(20)->create();
-        \App\Models\TipoPublicacion::factory(20)->create();
-        \App\Models\PublicacionInformativa::factory(20)
+        \App\Models\Etiqueta::factory(15)->create();
+        \App\Models\TipoDenuncia::factory(15)->create();
+        \App\Models\TipoPublicacion::factory(15)->create();
+        \App\Models\PublicacionInformativa::factory(15)
             ->hasImagens(1)
             ->hasDenuncias(5)->create();
-        \App\Models\Mascota::factory(20)
+        \App\Models\Mascota::factory(15)
             ->hasImagens(3)
             ->hasEtiquetas(3)
             ->hasSeguimientos(5)
