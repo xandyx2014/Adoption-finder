f1bbf9d1290d161ecdd161fcd5de99c0e555ee70
xandyx2014 <xandyx2014@gmail.com>
2021-04-25 14:56:23 -0400

    Revert "add force https"
    This reverts commit b504f4358bcf24c698e98e0daa1ab0277f8369b9.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,7 @@ namespace App\Providers;
 use App\Models\Permiso;
 use App\Models\User;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
@@ -30,9 +28,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (App::environment('production')) {
-            URL::forceScheme('https');
-        }
        /* Carbon::setUTF8(true);
         Carbon::setLocale(config('app.locale'));
         setlocale(LC_TIME, config('app.locale'));*/
