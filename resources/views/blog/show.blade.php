9ea9209baeb291510f902bb5f33e1ce7d6afd7ee
xandyx2014 <xandyx2014@gmail.com>
2021-05-04 15:37:50 -0400

    add text color to blog show

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -4,12 +4,12 @@
     <main role="main" class="container">
         <div class="row">
             <div class="col-md-8 blog-main">
-                <h3 class="pb-3 mb-4 font-italic border-bottom">
+                <h3 class="pb-3 mb-4 font-italic border-bottom text-primary">
                     {{ $publicacion->titulo  }}
                 </h3>
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">{{ $publicacion->subtitulo  }}</h2>
+                    <h2 class="blog-post-title text-primary">{{ $publicacion->subtitulo  }}</h2>
                     <p class="blog-post-meta"> {{ \Illuminate\Support\Carbon::parse( $publicacion->created_at)->diffForHumans() }} <a
                             href="#">{{ $publicacion->user->name }}</a></p>
                     <div>
